6d152afcf686b5c828ae134a159d514526a35579
Alan Shabrandi <alan.shabrandi@gmail.com>
2025-11-14 21:55:37 +0330

    🐳 chore: register API routes and configure DRF authentication

---
+++ chat/urls.py
@@ -0,0 +1,8 @@
+from rest_framework.routers import DefaultRouter
+from .views import ConversationViewSet, MessageViewSet
+
+router = DefaultRouter()
+router.register(r'conversation', ConversationViewSet, basename="conversation")
+router.register(r'message', MessageViewSet, basename="message")
+
+urlpatterns = router.urls
